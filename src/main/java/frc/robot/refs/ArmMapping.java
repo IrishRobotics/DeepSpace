70eff206efb91d50e74310dce809173e26f94a05
Sean Witte <Seanmwitte@gmail.com>
2019-01-16 23:05:31 -0600

    Figured out Maps for Arm position mapping

--- src/main/java/frc/robot/refs/ArmMapping.java
+++ src/main/java/frc/robot/refs/ArmMapping.java
@@ -1,8 +1,13 @@
 package frc.robot.refs;
 
-import java.util.HashMap;
+import java.util.Map;
 
 public class ArmMapping {
-    public HashMap armMapping = new HashMap(540);
-    
+    //public HashMap<Integer, String> armMap = new HashMap<>(540);
+    public Map<Integer, String> armMap = Map.of(
+        1, "Length:Height",
+        2, "Length:Height",
+        3, "Length:Height",
+        4, "Length:Height"
+    );
 }
